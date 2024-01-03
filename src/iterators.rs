ea194b88c1e5ff04161b5ac63eb5c4a30e4a6b70
Tap <46947072+ttappr@users.noreply.github.com>
2024-01-03 11:44:05 -0800

    Moved iterator tests to iterators.rs.

--- src/iterators.rs
+++ src/iterators.rs
@@ -1,21 +1,47 @@
+//! # Iterators, The Iterators Used in TrieMap
+//! 
+//! Each iterator implements the InnerIter trait, which provides the traversal
+//! algorithms. 
+//! 
+
 use crate::trie_map::*;
 
+/// The common trait for all iterators that implements the traversal algorithm.
+/// Each iterator implements the basic accessor methods, which simplfies their
+/// implementation by eliminating duplicated code. The `inner_next` and
+/// `inner_next_back` methods are the core of the traversal algorithm and 
+/// mirror Iterator::next and Iterator::next_back.
+/// 
 trait InnerIter<V, const R: usize, const B: u8> {
     type Item;
 
+    /// Returns a stack used to track where in the trie the iterator is.
+    /// 
     fn stack(&mut self) -> &mut Vec<(NodeHandle, usize, bool)>;
     
+    /// Returns a vector used to assemble the keys during traversal.
+    /// 
     fn key(&mut self) -> &mut Vec<u8>;
     
+    /// Returns a reference to the iterator's trie's values field.
+    /// 
     fn values(&self) -> &Vec<Option<V>>;
     
+    /// Invokes the iterator's trie's hderef method.
+    /// 
     fn hderef(&self, handle: NodeHandle) -> &Node<R>;
     
+    /// Invoked on the iterator to get it to produce the next iterator item.
+    /// 
     fn iter_item<'a>(&'a mut self, 
                      hcurr : NodeHandle,
                      hval  : ValueHandle,
                      key   : Box<[u8]>  ) -> Option<Self::Item>;
 
+    /// The core of the traversal algorithm. This method is invoked by the
+    /// iterator's next method. Each iterator implements Iterator and its 
+    /// next() method which onlly calls this method.
+    /// 
     fn inner_next(&mut self) -> Option<Self::Item> {
         if let Some(next) = self.stack().last_mut() {
             if next.1 == usize::MAX {
@@ -44,6 +70,11 @@ trait InnerIter<V, const R: usize, const B: u8> {
         None
     }
 
+    /// The core of the traversal algorithm. This method is invoked by the
+    /// iterator's next_back method. Each iterator implements 
+    /// DoubleEndedIterator and its next_back() method which onlly calls this 
+    /// method.
+    /// 
     fn inner_next_back(&mut self) -> Option<Self::Item> {
         if let Some(next) = self.stack().last_mut() {
             if next.1 == usize::MAX {
@@ -72,6 +103,8 @@ trait InnerIter<V, const R: usize, const B: u8> {
     }
 }
 
+/// A consuming iterator over the key-value pairs of a `TrieMap`.
+/// 
 pub struct IntoIter<V, const R: usize, const B: u8> {
     trie  : TrieMap<V, R, B>,
     key   : Vec<u8>,
@@ -137,6 +170,9 @@ impl<V, const R: usize, const B: u8> IntoIterator for TrieMap<V, R, B> {
     }
 }
 
+/// An iterator over the key-value pairs of a `TrieMap` that holds an 
+/// immutable reference to the trie.
+/// 
 pub struct Iter<'a, V, const R: usize, const B: u8> {
     trie  : &'a TrieMap<V, R, B>,
     key   : Vec<u8>,
@@ -206,6 +242,9 @@ impl<'a, V, const R: usize, const B: u8> IntoIterator for &'a TrieMap<V, R, B> {
     }
 }
 
+/// An iterator over the key-value pairs of a `TrieMap` that holds a mutable
+/// reference to the trie. It produces items that can be modified by the caller.
+/// 
 pub struct IterMut<'a, V, const R: usize, const B: u8> {
     trie  : &'a mut TrieMap<V, R, B>,
     key   : Vec<u8>,
@@ -281,6 +320,9 @@ impl <'a, V, const R: usize, const B: u8> IntoIterator
     }
 }
 
+/// An iterator over the keys of a `TrieMap` that holds an immutable reference
+/// to the trie.
+/// 
 pub struct Keys<'a, V, const R: usize, const B: u8> {
     iter: Iter<'a, V, R, B>,
 }
@@ -299,6 +341,9 @@ impl<'a, V, const R: usize, const B: u8> Iterator for Keys<'a, V, R, B> {
     }
 }
 
+/// An iterator over the values of a `TrieMap` that holds an immutable reference
+/// to the trie.
+/// 
 pub struct Values<'a, V, const R: usize, const B: u8> {
     trie  : &'a TrieMap<V, R, B>,
     idx   : usize,
@@ -327,6 +372,225 @@ impl<'a, V, const R: usize, const B: u8> Iterator for Values<'a, V, R, B> {
             self.trie.values[self.idx - 1].as_ref()
         } else {
             None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn bx(slice: &[u8]) -> Box<[u8]> {
+        slice.to_vec().into_boxed_slice()
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+        trie.insert(b"hello", 1);
+        trie.insert(b"world", 2);
+
+        let mut iter = trie.into_iter();
+
+        assert_eq!(iter.next(), Some((bx(b"hello"), 1)));
+        assert_eq!(iter.next(), Some((bx(b"world"), 2)));
+        assert_eq!(iter.next(), None);
+
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        trie.insert(b"hello", 1);
+        trie.insert(b"world", 2);
+        trie.insert(b"hellowalterjack", 3);
+
+        let mut iter = trie.into_iter();
+
+        assert_eq!(iter.next(), Some((bx(b"hello"), 1)));
+        assert_eq!(iter.next(), Some((bx(b"hellowalterjack"), 3)));
+        assert_eq!(iter.next(), Some((bx(b"world"), 2)));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn into_iter_rev() {
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+        trie.insert(b"hello", 1);
+        trie.insert(b"world", 2);
+
+        let mut iter = trie.into_iter();
+
+        assert_eq!(iter.next_back(), Some((bx(b"world"), 2)));
+        assert_eq!(iter.next_back(), Some((bx(b"hello"), 1)));
+        assert_eq!(iter.next_back(), None);
+
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        trie.insert(b"hello", 1);
+        trie.insert(b"world", 2);
+        trie.insert(b"hellowalterjack", 3);
+
+        let mut iter = trie.into_iter().rev();
+
+        assert_eq!(iter.next(), Some((bx(b"world"), 2)));
+        assert_eq!(iter.next(), Some((bx(b"hellowalterjack"), 3)));
+        assert_eq!(iter.next(), Some((bx(b"hello"), 1)));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        let data = [(b"hello", 1), (b"world", 2)];
+
+        for (key, val) in &data {
+            trie.insert(key, *val);
+        }
+        for ((key1, &val1), (key2, val2)) in trie.iter().zip(data) {
+            assert_eq!(key1.as_ref(), key2);
+            assert_eq!(val1, val2);
+        }
+
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        let data = [("hello", 1), ("hellowalterjack", 2), ("world", 3)];
+
+        for &(key, val) in &data {
+            trie.insert(key, val);
+        }
+        for ((key1, &val1), (key2, val2)) in trie.iter().zip(data) {
+            assert_eq!(key1.as_ref(), key2.as_bytes());
+            assert_eq!(val1, val2);
+        }
+    }
+
+    #[test]
+    fn iter_rev() {
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+        trie.insert(b"hello", 1);
+        trie.insert(b"world", 2);
+
+        let mut iter = trie.iter();
+
+        assert_eq!(iter.next_back(), Some((bx(b"world"), &2)));
+        assert_eq!(iter.next_back(), Some((bx(b"hello"), &1)));
+        assert_eq!(iter.next_back(), None);
+
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        trie.insert(b"hello", 1);
+        trie.insert(b"world", 2);
+        trie.insert(b"hellowalterjack", 3);
+
+        let mut iter = trie.iter().rev();
+
+        assert_eq!(iter.next(), Some((bx(b"world"), &2)));
+        assert_eq!(iter.next(), Some((bx(b"hellowalterjack"), &3)));
+        assert_eq!(iter.next(), Some((bx(b"hello"), &1)));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        let data = [(b"hello", 1), (b"world", 2)];
+
+        for (key, val) in &data {
+            trie.insert(key, *val);
+        }
+        for ((key1, val1), (key2, val2)) in trie.iter_mut().zip(data) {
+            assert_eq!(key1.as_ref(), key2);
+            assert_eq!(*val1, val2);
+            *val1 = 17;
+        }
+        for ((key1, &val1), (key2, _val2)) in trie.iter().zip(data) {
+            assert_eq!(key1.as_ref(), key2);
+            assert_eq!(val1, 17);
+        }
+
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        let data = [("hello", 1), ("hellowalterjack", 2), ("world", 3)];
+
+        for &(key, val) in &data {
+            trie.insert(key, val);
+        }
+        for ((key1, val1), (key2, val2)) in trie.iter_mut().zip(data) {
+            assert_eq!(key1.as_ref(), key2.as_bytes());
+            assert_eq!(*val1, val2);
+            *val1 = 17;
+        }
+        for ((key1, &val1), (key2, _val2)) in trie.iter().zip(data) {
+            assert_eq!(key1.as_ref(), key2.as_bytes());
+            assert_eq!(val1, 17);
+        }
+    }
+
+    #[test]
+    fn iter_mut_rev() {
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        let data = [(b"hello", 1), (b"world", 2)];
+
+        for (key, val) in &data {
+            trie.insert(key, *val);
+        }
+        let iter = trie.iter_mut().rev().zip(data.into_iter().rev());
+
+        for ((key1, val1), (key2, val2)) in iter {
+            assert_eq!(key1.as_ref(), key2);
+            assert_eq!(*val1, val2);
+            *val1 = 17;
+        }
+        for ((key1, &val1), (key2, _val2)) in trie.iter().zip(data) {
+            assert_eq!(key1.as_ref(), key2);
+            assert_eq!(val1, 17);
+        }
+
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        let data = [("hello", 1), ("hellowalterjack", 2), ("world", 3)];
+
+        for &(key, val) in &data {
+            trie.insert(key, val);
+        }
+        let iter = trie.iter_mut().rev().zip(data.into_iter().rev());
+
+        for ((key1, val1), (key2, val2)) in iter {
+            assert_eq!(key1.as_ref(), key2.as_bytes());
+            assert_eq!(*val1, val2);
+            *val1 = 17;
+        }
+        for ((key1, &val1), (key2, _val2)) in trie.iter().zip(data) {
+            assert_eq!(key1.as_ref(), key2.as_bytes());
+            assert_eq!(val1, 17);
+        }
+    }
+
+    #[test]
+    fn keys() {
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        let data = [(b"hello", 1), (b"world", 2)];
+
+        for (key, val) in &data {
+            trie.insert(key, *val);
+        }
+        for (key1, key2) in trie.keys().zip(data.iter().map(|(k, _)| k)) {
+            assert_eq!(key1.as_ref(), *key2);
+        }
+
+        let mut trie: TrieMap<i32, 26, b'a'> = TrieMap::new();
+
+        let data = [("hello", 1), ("hellowalterjack", 2), ("world", 3)];
+
+        for &(key, val) in &data {
+            trie.insert(key, val);
+        }
+        let iter = trie.keys().zip(data.iter().map(|(k, _)| k.as_bytes()));
+
+        for (key1, key2) in iter {
+            assert_eq!(key1.as_ref(), key2);
         }
     }
 }
